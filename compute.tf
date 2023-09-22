217610164140e119fd5b0fd4e402f5faca9343ec
asarkar157 <20803896+asarkar157@users.noreply.github.com>
2023-09-22 14:30:17 -0700

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -44,7 +44,7 @@ resource "oci_core_instance" "hoodrat-1" {
     assign_private_dns_record = "true"
     assign_public_ip          = "true" # this instance has a Public IP
     hostname_label            = "hoodrat-1"
-    subnet_id                 = data.oci_core_subnet.subnet_0.id
+    subnet_id                 = data.oci_core_subnet.subnet_0.subnet_id
 
     #subnet_id : ocid1.subnet.oc1.phx.aaaaaaaa4uwwmqsqqfqgkymn4xlbvubqshxskfpljuzf4ccxrdg5nmp4l7ha
   }
