0a59db4b4d17ee11df2de08e3cb1b3d9a33210ee
asarkar157 <20803896+asarkar157@users.noreply.github.com>
2023-09-22 04:59:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,27 @@
-provider "oci" {}
+variable "region" {
+}
+
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+  default = ""
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+  default = ""
+}
+
+provider "oci" {
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
 
 variable "github_user" {
   type = string
